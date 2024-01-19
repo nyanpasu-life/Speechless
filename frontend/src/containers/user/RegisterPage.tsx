130b620d8c0c52f98aa0c768bd5ec129a793c855
김대현 <seol3964@naver.com>
2024-01-19 13:18:02 +0900

    [FE] [S10P12A806-76] 프로젝트 구성 및 기본 설정

---
+++ frontend/src/containers/user/RegisterPage.tsx
@@ -0,0 +1,14 @@
+export const RegisterPage = () => {
+	return (
+		<>
+			<h1>Register</h1>
+			<p>Register page content</p>
+
+			<p>
+				Edit <code>src/containers/user/RegisterPage.tsx</code> and save to test HMR
+			</p>
+
+			<p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
+		</>
+	);
+};
