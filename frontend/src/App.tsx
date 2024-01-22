ef4b4028190683ab5e53b2b16d3b8314a19a12d1
김대현 <seol3964@naver.com>
2024-01-22 13:49:12 +0900

    [FE] [S10P12A806-76] 프로젝트 및 전역 레이아웃 구성

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,32 @@
 import './App.css';
 
-import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { IndexPage } from './containers/layout/IndexPage.tsx';
+import { Header } from './containers/layout/Header.tsx';
+import { Footer } from './containers/layout/Footer.tsx';
+import { createTheme, ThemeProvider } from '@mui/material';
 
-const colors = {
-	primary: {
-		// TODO: Design System Color 정하기
-		100: '#E2E2FF',
-		200: '#8884FF',
-		300: '#38369A',
-		400: '#1D1D6E',
-		500: '#131357',
-	}
-}
-
-const theme = extendTheme({ colors });
+const theme = createTheme({
+	palette: {
+		primary: {
+			main: '#9F7AEA'
+		},
+		secondary: {
+			main: '#FFFFFF'
+		}
+	},
+});
 
 export default function App() {
 	return (
-		<ChakraProvider resetCSS theme={theme}>
-
-		</ChakraProvider>
+		<ThemeProvider theme={theme}>
+			<Header></Header>
+			<BrowserRouter>
+				<Routes>
+					<Route path='/' element={<IndexPage />} />
+				</Routes>
+			</BrowserRouter>
+			<Footer></Footer>
+		</ThemeProvider>
 	);
 }
