1cb16b36ce66194b3dfd21e488f87dae60760712
명소이 <myeongsoyi@gmail.com>
2024-01-30 17:46:23 +0900

    fix: build.gradle, Util 수정
    - build.gradle, Util 수정
    
    Change-Id: I2da0800283b067762f44996a2fceafbabfd695d9

--- backend/src/main/java/speechless/session/storage/utils/FileUtil.java
+++ backend/src/main/java/speechless/session/storage/utils/FileUtil.java
@@ -1,16 +1,6 @@
 package speechless.session.storage.utils;
 
-<<<<<<< HEAD
-import static speechless.session.storage.config.StorageCredential.bucketName;
-import static speechless.session.storage.config.StorageCredential.setS3;
-
 import com.amazonaws.SdkClientException;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.AmazonS3Exception;
-import java.io.File;
-=======
-import com.amazonaws.SdkClientException;
-import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.client.builder.AwsClientBuilder;
@@ -19,36 +9,30 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.AmazonS3Exception;
 import java.io.File;
 import org.springframework.beans.factory.annotation.Value;
->>>>>>> feature/193
 import org.springframework.stereotype.Component;
 import speechless.session.storage.exception.StorageClientException;
 import speechless.session.storage.exception.StorageServiceException;
 
 @Component
 public class FileUtil {
-<<<<<<< HEAD
 
-    private static AmazonS3 s3 = setS3();
-=======
     final static String endPoint = "https://kr.object.ncloudstorage.com";
     final static String regionName = "kr-standard";
     private static String accessKey;
     private static String secretKey;
 
     public static String bucketName;
->>>>>>> feature/193
 
     public static void uploadFile(String id, String classification, String session, String take,
         String path) {
 
-<<<<<<< HEAD
-=======
         final AmazonS3 s3 = AmazonS3ClientBuilder.standard()
-            .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(endPoint, regionName))
-            .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey)))
+            .withEndpointConfiguration(
+                new AwsClientBuilder.EndpointConfiguration(endPoint, regionName))
+            .withCredentials(
+                new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey)))
             .build();
 
->>>>>>> feature/193
         String objectName = id + "_" + classification + "_" + session + "/" + take;
         String filePath = path;
 
@@ -63,14 +47,13 @@ public class FileUtil {
 
     public static void deleteFile(String id, String classification, String session, String take) {
 
-<<<<<<< HEAD
-=======
         final AmazonS3 s3 = AmazonS3ClientBuilder.standard()
-            .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(endPoint, regionName))
-            .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey)))
+            .withEndpointConfiguration(
+                new AwsClientBuilder.EndpointConfiguration(endPoint, regionName))
+            .withCredentials(
+                new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey)))
             .build();
 
->>>>>>> feature/193
         try {
             s3.deleteObject(bucketName, id + "_" + classification + "_" + session + "/" + take);
         } catch (AmazonS3Exception e) {
@@ -79,8 +62,6 @@ public class FileUtil {
             throw new StorageClientException();
         }
     }
-<<<<<<< HEAD
-=======
 
     @Value("${api-keys.storage.access-key}")
     private void setAccessKey(String key) {
@@ -96,5 +77,4 @@ public class FileUtil {
     private void setBucketName(String name) {
         bucketName = name;
     }
->>>>>>> feature/193
 }
--- backend/src/main/java/speechless/session/storage/utils/FolderUtil.java
+++ backend/src/main/java/speechless/session/storage/utils/FolderUtil.java
@@ -1,40 +1,23 @@
 package speechless.session.storage.utils;
 
-<<<<<<< HEAD
-import static speechless.session.storage.config.StorageCredential.bucketName;
-import static speechless.session.storage.config.StorageCredential.setS3;
-
 import com.amazonaws.SdkClientException;
-import com.amazonaws.services.s3.AmazonS3;
-=======
-import com.amazonaws.SdkClientException;
-import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
->>>>>>> feature/193
 import com.amazonaws.services.s3.model.AmazonS3Exception;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import java.io.ByteArrayInputStream;
-<<<<<<< HEAD
-=======
 import org.springframework.beans.factory.annotation.Value;
->>>>>>> feature/193
 import org.springframework.stereotype.Component;
 import speechless.session.storage.exception.StorageClientException;
 import speechless.session.storage.exception.StorageServiceException;
 
 @Component
 public class FolderUtil {
-<<<<<<< HEAD
 
-    private static AmazonS3 s3 = setS3();
-
-    public static void createFolder(String id, String classification, String session) {
-=======
     final static String endPoint = "https://kr.object.ncloudstorage.com";
     final static String regionName = "kr-standard";
     private static String accessKey;
@@ -45,11 +28,12 @@ public class FolderUtil {
     public static void createFolder(String id, String classification, String session) {
 
         final AmazonS3 s3 = AmazonS3ClientBuilder.standard()
-            .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(endPoint, regionName))
-            .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey)))
+            .withEndpointConfiguration(
+                new AwsClientBuilder.EndpointConfiguration(endPoint, regionName))
+            .withCredentials(
+                new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey)))
             .build();
 
->>>>>>> feature/193
         String folderName = id + "_" + classification + "_" + session;
 
         ObjectMetadata objectMetadata = new ObjectMetadata();
@@ -69,14 +53,13 @@ public class FolderUtil {
 
     public static void deleteFolder(String id, String classification, String session) {
 
-<<<<<<< HEAD
-=======
         final AmazonS3 s3 = AmazonS3ClientBuilder.standard()
-            .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(endPoint, regionName))
-            .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey)))
+            .withEndpointConfiguration(
+                new AwsClientBuilder.EndpointConfiguration(endPoint, regionName))
+            .withCredentials(
+                new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey)))
             .build();
 
->>>>>>> feature/193
         try {
             s3.deleteObject(bucketName, id + "_" + classification + "_" + session);
         } catch (AmazonS3Exception e) {
@@ -85,8 +68,6 @@ public class FolderUtil {
             throw new StorageClientException();
         }
     }
-<<<<<<< HEAD
-=======
 
     @Value("${api-keys.storage.access-key}")
     private void setAccessKey(String key) {
@@ -102,5 +83,4 @@ public class FolderUtil {
     private void setBucketName(String name) {
         bucketName = name;
     }
->>>>>>> feature/193
 }
