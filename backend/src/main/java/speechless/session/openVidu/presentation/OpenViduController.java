6037a6371ae2df0db5ef3e6ca1e370e15deb4173
명소이 <myeongsoyi@gmail.com>
2024-01-31 11:16:34 +0900

    [BE][S10P12A806-247] OpenVidu application server
    commit ea6165e790f6de990fb02d75d8dd5a07f6304898
    Author: 명소이 <myeongsoyi@gmail.com>
    Date:   Wed Jan 31 11:06:45 2024 +0900
    
        fix: openvidu application server 수정
    
        - 오류 수정
    
        Change-Id: I538d448e55ce71c90de525004c1a0017074e7cfd
        Related to: S10P12A806-247
    
    commit 4e8369fdb1ab080bc84db8e2b88e51f7e5d63262
    Author: 명소이 <myeongsoyi@gmail.com>
    Date:   Tue Jan 30 11:09:44 2024 +0900
    
        feat: OpenVidu application, presentation 생성
    
        - openvidu application, presentation 생성
    
        Change-Id: I3e496db68441d107b803dd31bbd9d6c335908b32
        Related to: S10P12A806-247

--- backend/src/main/java/speechless/session/openVidu/presentation/OpenViduController.java
+++ backend/src/main/java/speechless/session/openVidu/presentation/OpenViduController.java
@@ -33,69 +33,69 @@ import speechless.session.openVidu.application.SessionIdService;
 @RequestMapping("/openvidu")
 public class OpenViduController {
 
-	@Value("${OPENVIDU_URL}")
-	private String OPENVIDU_URL;
+    @Value("${OPENVIDU_URL}")
+    private String OPENVIDU_URL;
 
-	@Value("${OPENVIDU_SECRET}")
-	private String OPENVIDU_SECRET;
+    @Value("${OPENVIDU_SECRET}")
+    private String OPENVIDU_SECRET;
 
-	private OpenVidu openvidu;
+    private OpenVidu openvidu;
 
-	@Autowired
-	private SessionIdService sessionIdService;
+    @Autowired
+    private SessionIdService sessionIdService;
 
-	@PostConstruct
-	public void init() {
-		this.openvidu = new OpenVidu(OPENVIDU_URL, OPENVIDU_SECRET);
-	}
+    @PostConstruct
+    public void init() {
+        this.openvidu = new OpenVidu(OPENVIDU_URL, OPENVIDU_SECRET);
+    }
 
-	@PostMapping("/sessions")
-	@Operation(summary = "새로운 비디오 세션을 초기화합니다.",
-		description = "필요한 경우 세션 설정을 포함하여 새로운 비디오 세션을 생성합니다.")
-	@ApiResponse(responseCode = "200", description = "새로운 세션 ID를 반환합니다.")
-	@ApiResponse(responseCode = "401", description = "인증 실패, 비밀번호 불일치")
-	public ResponseEntity<String> initializeSession(
-		@RequestBody(required = false) @Parameter(description = "세션 생성에 사용될 설정 값. 옵션으로 지정 가능.") Map<String, Object> params)
-		throws OpenViduJavaClientException, OpenViduHttpException {
-		SessionProperties properties = SessionProperties.fromJson(params).build();
-		Session session = openvidu.createSession(properties);
-		return new ResponseEntity<>(session.getSessionId(), HttpStatus.OK);
-	}
+    @PostMapping("/sessions")
+    @Operation(summary = "새로운 비디오 세션을 초기화합니다.",
+        description = "필요한 경우 세션 설정을 포함하여 새로운 비디오 세션을 생성합니다.")
+    @ApiResponse(responseCode = "200", description = "새로운 세션 ID를 반환합니다.")
+    @ApiResponse(responseCode = "401", description = "인증 실패, 비밀번호 불일치")
+    public ResponseEntity<String> initializeSession(
+        @RequestBody(required = false) @Parameter(description = "세션 생성에 사용될 설정 값. 옵션으로 지정 가능.") Map<String, Object> params)
+        throws OpenViduJavaClientException, OpenViduHttpException {
+        SessionProperties properties = new SessionProperties.Builder().build();
+        Session session = openvidu.createSession(properties);
+        return new ResponseEntity<>(session.getSessionId(), HttpStatus.OK);
+    }
 
-	@PostMapping("/sessions/{sessionId}/connections")
-	@Operation(summary = "지정된 세션 ID에 대한 새로운 연결을 생성합니다.",
-		description = "지정된 세션에 참여할 수 있는 새로운 연결을 생성합니다.")
-	@ApiResponse(responseCode = "200", description = "새로운 연결 토큰을 반환합니다.")
-	@ApiResponse(responseCode = "404", description = "세션을 찾을 수 없습니다.")
-	public ResponseEntity<String> createConnection(@PathVariable("sessionId") String sessionId,
-		@RequestBody(required = false) @Parameter(description = "연결 생성에 사용될 설정 값. 옵션으로 지정 가능.") Map<String, Object> params)
-		throws OpenViduJavaClientException, OpenViduHttpException {
-		Session session = openvidu.getActiveSession(sessionId);
-		if (session == null) {
-			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-		}
-		ConnectionProperties properties = ConnectionProperties.fromJson(params).build();
-		Connection connection = session.createConnection(properties);
-		return new ResponseEntity<>(connection.getToken(), HttpStatus.OK);
-	}
+    @PostMapping("/sessions/{sessionId}/connections")
+    @Operation(summary = "지정된 세션 ID에 대한 새로운 연결을 생성합니다.",
+        description = "지정된 세션에 참여할 수 있는 새로운 연결을 생성합니다.")
+    @ApiResponse(responseCode = "200", description = "새로운 연결 토큰을 반환합니다.")
+    @ApiResponse(responseCode = "404", description = "세션을 찾을 수 없습니다.")
+    public ResponseEntity<String> createConnection(@PathVariable("sessionId") String sessionId,
+        @RequestBody(required = false) @Parameter(description = "연결 생성에 사용될 설정 값. 옵션으로 지정 가능.") Map<String, Object> params)
+        throws OpenViduJavaClientException, OpenViduHttpException {
+        Session session = openvidu.getActiveSession(sessionId);
+        if (session == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        ConnectionProperties properties = ConnectionProperties.fromJson(params).build();
+        Connection connection = session.createConnection(properties);
+        return new ResponseEntity<>(connection.getToken(), HttpStatus.OK);
+    }
 
-	@PostMapping("/session-id")
-	@Operation(summary = "고유한 세션 ID를 생성합니다.",
-		description = "새로운 고유한 세션 ID를 생성합니다.")
-	@ApiResponse(responseCode = "201", description = "생성된 고유 세션 ID를 반환합니다.")
-	public ResponseEntity<String> sessionId() {
-		String uuid = sessionIdService.createUuid();
-		return new ResponseEntity<>(uuid, HttpStatus.CREATED);
-	}
+    @PostMapping("/session-id")
+    @Operation(summary = "고유한 세션 ID를 생성합니다.",
+        description = "새로운 고유한 세션 ID를 생성합니다.")
+    @ApiResponse(responseCode = "201", description = "생성된 고유 세션 ID를 반환합니다.")
+    public ResponseEntity<String> sessionId() {
+        String uuid = sessionIdService.createUuid();
+        return new ResponseEntity<>(uuid, HttpStatus.CREATED);
+    }
 
-	@ExceptionHandler(OpenViduJavaClientException.class)
-	public ResponseEntity<String> handleClientException(OpenViduJavaClientException e) {
-		return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
-	}
+    @ExceptionHandler(OpenViduJavaClientException.class)
+    public ResponseEntity<String> handleClientException(OpenViduJavaClientException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
 
-	@ExceptionHandler(OpenViduHttpException.class)
-	public ResponseEntity<String> handleHttpException(OpenViduHttpException e) {
-		return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-	}
+    @ExceptionHandler(OpenViduHttpException.class)
+    public ResponseEntity<String> handleHttpException(OpenViduHttpException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
 
