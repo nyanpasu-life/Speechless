bd6011f4ac54cb1515491ac0f264317fe93626a7
한태희 <xotpqnd@gmail.com>
2024-02-01 16:50:49 +0900

    Revert "fix: Banner.tsx 머지 실수 수정"
    This reverts commit 711dfd97747bfa4c30634404be3bc2820bd0d89c.

--- frontend/src/components/Banners.tsx
+++ frontend/src/components/Banners.tsx
@@ -1,9 +1,22 @@
 import { useAuthStore } from "../stores/auth";
 import Banner1Img from '../assets/images/human_face.png';
 import Banner2Img from '../assets/images/human_robot_talk.png';
+import Banner1Img from '../assets/images/human_face.png';
+import Banner2Img from '../assets/images/human_robot_talk.png';
+
 export const Banner1 = () => {
+
     return(
     <>
+        <div className='flex items-center w-5/6 p-12 m-5 gap-10 border-2 rounded-3xl mx-auto'>
+            <div className='w-1/3'>
+                <img src={Banner1Img} alt='img' />
+            </div>
+            <div className='w-2/3'>
+                <p className='text-4xl'>
+                    Speechless: <br/>
+                    AI 분석과 함께하는 면접, 발표 연습
+                </p>
         <div className='border-2 rounded-xl'>
             <div className="flex flex-row">
                 <div className='w-1/3'>
@@ -20,10 +33,23 @@ export const Banner1 = () => {
     </>
     )
 }
+
 export const Banner2 = () => {
     const authStore = useAuthStore();
+
     return(
     <>
+        <div className='flex items-center w-5/6 p-12 m-5 gap-10 border-2 rounded-3xl mx-auto'>
+            <div className='w-1/5'>
+                <img src={Banner2Img} alt='img' />
+            </div>
+            <div className=''>
+                <p className='text-2xl'>
+                    기업에 지원한 자기소개서를 기반으로<br/>
+                    면접 연습을 할 수 있습니다. <br/>
+                    자기소개서를 추가하고 <br/>
+                    {authStore.name?authStore.name:'사용자'}님에게 맞는 면접 연습을 시작하세요!
+                </p>
         <div className='border-2 rounded-xl'>
             <div className="flex flex-row">
                 <div className='w-1/3'>
