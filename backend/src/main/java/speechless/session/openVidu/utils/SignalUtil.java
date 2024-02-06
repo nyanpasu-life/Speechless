f9b2cb8e1b73a29df37f5d2251f476cfe9f953f7
이인석 <inkcloud365@gmail.com>
2024-02-06 16:51:57 +0900

    feat: add signal logging

--- backend/src/main/java/speechless/session/openVidu/utils/SignalUtil.java
+++ backend/src/main/java/speechless/session/openVidu/utils/SignalUtil.java
@@ -1,12 +1,14 @@
 package speechless.session.openVidu.utils;
 
 import java.util.Base64;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 import speechless.session.openVidu.dto.Signal;
 
+@Slf4j
 @Component
 public class SignalUtil {
 
@@ -18,12 +20,15 @@ public class SignalUtil {
 
     public void sendSignal(Signal params) {
         RestClient client = RestClient.create();
-        client.post()
+        String temp= client.post()
             .uri(OPENVIDU_URL + "openvidu/signal")
             .header("Authorization",
                 "Basic " + Base64.getEncoder().encodeToString(OPENVIDU_SECRET.getBytes()))
             .contentType(MediaType.APPLICATION_JSON)
             .body(params)
-            .retrieve();
+            .retrieve()
+            .body(String.class);
+
+        log.info(temp);
     }
 }
