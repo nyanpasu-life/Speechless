2cd806f44937fd4e07745f7df720fcbb14bf470d
배정훈 <bjh4395@naver.com>
2024-02-13 15:03:33 +0900

    refactor: Member을 찾을수 없을때 401에러를 반환하도록 수정

--- backend/src/main/java/speechless/member/exception/MemberNotFoundException.java
+++ backend/src/main/java/speechless/member/exception/MemberNotFoundException.java
@@ -4,9 +4,10 @@ import speechless.common.error.ErrorCode;
 import speechless.common.error.SpeechlessException;
 
 import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 
 public class MemberNotFoundException extends SpeechlessException {
     public MemberNotFoundException() {
-        super(new ErrorCode(NOT_FOUND, "회원을 찾을 수 없습니다!"));
+        super(new ErrorCode(UNAUTHORIZED, "회원을 찾을 수 없습니다!"));
     }
 }
