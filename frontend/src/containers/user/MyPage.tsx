cf2698ef3687ee3f156648aebe4d9a86bb69820b
한태희 <xotpqnd@gmail.com>
2024-02-14 10:33:42 +0900

    feat: 마이페이지 점수 추이 그래프 추가, 회원탈퇴 버튼 제거
    Squashed commit of the following:
    
    commit 12f33ea57d2bb5a29f6460e8d5196be278674a86
    Author: 한태희 <xotpqnd@gmail.com>
    Date:   Wed Feb 14 09:07:41 2024 +0900
    
        feat: 마이페이지 점수 추이 그래프 추가, 회원탈퇴 버튼 제거
    
        Change-Id: I3ee64e06e9c1fd9a6ccb88b2ff99e88e6bb932ff

--- frontend/src/containers/user/MyPage.tsx
+++ frontend/src/containers/user/MyPage.tsx
@@ -1,21 +1,10 @@
 import React from 'react';
-import { Button, Card, Avatar } from 'flowbite-react';
+import { Card, Avatar } from 'flowbite-react';
 import { useAuthStore } from '../../stores/auth';
 
-import {
-	Chart as ChartJS,
-	CategoryScale,
-	LinearScale,
-	PointElement,
-	LineElement,
-	Title,
-	Tooltip,
-	Legend,
-} from 'chart.js';
-import { Line } from 'react-chartjs-2';
 import { CustomButton } from '../../components/CustomButton.tsx';
+import { RecentScoreChart } from './RecentScoreChart.tsx';
 
-ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
 
 export const MyPage = () => {
 	const authStore = useAuthStore();
@@ -24,7 +13,7 @@ export const MyPage = () => {
 		<>
 			<Card className='w-3/4 mx-auto px-10 py-5'>
 				<div className='w-full flex flex-row'>
-					<div className='basis-1/3 flex flex-col gap-2 items-center'>
+					<div className='basis-1/3 gap-2 flex flex-col justify-center items-center'>
 						<Avatar
 							img={authStore.profileImage}
 							alt='https://flowbite.com/docs/images/people/profile-picture-5.jpg'
@@ -34,12 +23,11 @@ export const MyPage = () => {
 						<p className='text-2xl font-semibold tracking-tight'>{authStore.name}</p>
 						<p className='text-md font-semibold text-gray-600'>{authStore.email}</p>
 					</div>
-					<div className='basis-2/3'></div>
+					<div className='basis-2/3'>
+						<RecentScoreChart/>
+					</div>
 				</div>
 			</Card>
-			<div className='w-3/4 mx-auto flex justify-center mt-5'>
-				<CustomButton color='negative'>회원 데이터 삭제</CustomButton>
-			</div>
 		</>
 	);
 };
---
+++ frontend/src/containers/user/RecentScoreChart.tsx
@@ -0,0 +1,95 @@
+import React, { useEffect, useState } from 'react';
+import { useLocalAxios } from '../../utils/axios';
+import { TitledCard } from '../../components/TitledCard.tsx';
+
+import {
+	Chart as ChartJS,
+	CategoryScale,
+	LinearScale,
+	BarElement,
+	LineElement,
+	Title,
+	Tooltip,
+	Legend,
+    ChartData,
+	PointElement,
+} from 'chart.js';
+
+ChartJS.register(CategoryScale, LinearScale, PointElement,  BarElement, LineElement, Title, Tooltip, Legend);
+
+import { Line } from 'react-chartjs-2';
+
+export const RecentScoreChart = () => {
+
+	const localAxios = useLocalAxios(true);
+
+	const [graphData, setGraphData] = useState<ChartData<"line", number[], unknown>>({
+        labels: [],
+        datasets: [
+            {
+                label: '표정 점수',
+                data: [],
+                backgroundColor: getRandomColor(),
+                borderColor: getRandomColor(),
+                pointRadius: 0,
+                fill: false,
+            },
+        ],
+    });
+
+	useEffect(() => {
+		localAxios
+            .get('interview', { params: { pageSize: 10, pageNum: 1 } })
+            .then((res) => {
+                const infos = res.data.interviewInfos;
+                const faceYlist =  infos.map((info: { faceScore: number; }) => info.faceScore);
+				const pronunciationYlist =  infos.map((info: { pronunciationScore: number; }) => info.pronunciationScore);
+                const faceColor = getRandomColor();
+				const pronunciationColor = getRandomColor();
+                setGraphData({
+                    labels: new Array(faceYlist.length).fill(''),
+                    datasets: [
+                        {
+                            label: '표정 점수',
+                            data: faceYlist,
+							borderColor: faceColor,
+                            backgroundColor: faceColor,
+                            pointRadius: 0,
+                            fill: false,
+                        },
+						{
+                            label: '발음 점수',
+                            data: pronunciationYlist,
+							borderColor: pronunciationColor,
+                            backgroundColor: pronunciationColor,
+                            pointRadius: 0,
+                            fill: false,
+                        },
+                    ],
+                })
+            })
+	}, []);
+
+	function getRandomColor() {
+		const colorList = [
+			'rgba(255, 0, 0, 1)', // 빨강
+			'rgba(255, 165, 0, 1)', // 주황
+			'rgba(255, 255, 0, 1)', // 황색
+			'rgba(154, 205, 50, 1)', // 연두
+			'rgba(0, 128, 0, 1)', // 녹색
+			'rgba(0, 255, 255, 1)', // 청록
+			'rgba(0, 0, 255, 1)', // 청색
+			'rgba(128, 0, 128, 1)', // 보라
+			'rgba(0, 0, 0, 1)', // 검정
+		];
+
+		return colorList[Math.floor(Math.random() * colorList.length)];
+	}
+
+	return (
+		<div>
+			<p className='text-2xl font-semibold'>최근 점수 변화</p>
+            <Line data={graphData}></Line>
+		</div>
+	);
+};
